a44d0de5536123b1a81af8ed61d4eb46d197daff
Sergio Luiz Brito <sergiolmendes2026@gmail.com>
2026-08-10 19:36:01 -0300

    Update dsa_app.py

--- dsa_app.py
+++ dsa_app.py
@@ -198,11 +198,11 @@ with st.sidebar:
 
 selected = st.session_state.selected
 
-try:
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login("sergiolmendes2026@gmail.com", "jpxkrnphnxdzvibc")
-    print("E-mail enviado com sucesso!")
+def disparar_email_automatico(destinatario, arquivo_bytes, nome_arquivo):
+    servidor_smtp = "smtp.gmail.com"
+    porta = 587
+    remetente = "sergiolmendes2026@gmail.com"
+    senha = "jpxkrnphnxdzvibc"
 
     # ... resto do seu código de envio
     print("E-mail enviado com sucesso!")
