7c52ab99f96938d339d3a386196c6ce0b34c2452
Sergio Luiz Brito <sergiolmendes2026@gmail.com>
2026-08-10 18:53:27 -0300

    Update dsa_app.py

--- dsa_app.py
+++ dsa_app.py
@@ -211,7 +211,7 @@ def disparar_email_automatico(destinatario, arquivo_bytes, nome_arquivo):
     servidor_smtp = "smtp.gmail.com"
     porta = 587
     remetente = "sergiolmendes2026@gmail.com"
-    senha = "fknf jicm ttgx oqic"
+    senha = "fknfjicmttgxoqic"
 
     try:
         msg = MIMEMultipart()
