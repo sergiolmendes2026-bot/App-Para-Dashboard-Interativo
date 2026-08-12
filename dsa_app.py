cea764b476e6b82b568d9c04a553cc41e0bb477f
Sergio Luiz Brito <sergiolmendes2026@gmail.com>
2026-08-12 17:37:12 -0300

    Update dsa_app.py

--- dsa_app.py
+++ dsa_app.py
@@ -32,7 +32,7 @@ is_escuro = "Escuro" in st.session_state.tema_sistema
 
 bg_app = "#0e1117" if is_escuro else "#ffffff"
 text_app = "#ffffff" if is_escuro else "#1e293b"
-sidebar_bg = "#0b0f19" if is_escuro else "#f8fafc"
+sidebar_bg = "#0b0f19" if is_escuro else "#f8fafc" 
 
 # --- CSS E ESTILIZAÇÃO DO MENU E PAINEIS ---
 st.markdown(f"""
