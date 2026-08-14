ca3d302ef16fd5d9cfb42f1d0f73c28d030f5b84
Sergio Luiz Brito <sergiolmendes2026@gmail.com>
2026-08-14 18:35:20 -0300

    Update dsa_app.py

--- dsa_app.py
+++ dsa_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import sqlite3
-import plotly.express as px
+import plotly.express as px 
 import plotly.graph_objects as go
 from datetime import date, datetime
 import smtplib
