fea5c155e04e31824b3384e7e67732d290107eac
Sergio Luiz Brito <sergiolmendes2026@gmail.com>
2026-08-16 19:12:25 -0300

    Update dsa_app.py

--- dsa_app.py
+++ dsa_app.py
@@ -62,13 +62,12 @@ st.markdown(
             color: {text_app}; 
         }}
         
-        st.markdown("""
+      st.markdown("""
     <style>
     [data-testid="stSidebar"] { 
         background-color: #0b0f19 !important;
         box-shadow: 4px 0px 24px rgba(0, 0, 0, 0.45) !important;
         border-right: 1px solid rgba(255, 255, 255, 0.05);
-        padding: 0 !important;
     }
     </style>
     """, unsafe_allow_html=True)
